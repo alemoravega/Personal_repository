8a772229dff4a10324e421ef9e7965759cb29af6
donpollops5 <mora.vega.ale@gmail.com>
2023-12-25 23:28:33 -0300

    sigo con el resumen de python

--- Python/Unidad 1/2-1_Comparadores_logicos.py
+++ Python/Unidad 1/2-1_Comparadores_logicos.py
@@ -4,21 +4,9 @@
 # Version 0.0.0.0.0.0.1
 # DonPollo 2023 - All rights reserved
 
-# C O N V E R S I Ó N   V A R I A B L E S
+# C O M P A R A D O R E S   L Ó G I C O S :
 
 # ********************************************************************************************
 
-# Truthy y Falsy:
 
-# Espacio:
-print(bool(""))
-# String:
-print(bool("0"))
-# None
-print(bool(None))
-# Espacio mayor:
-print(bool(" "))
-# Número
-print(bool(0))
-
-# las únicas variables que bool entrega False son: "", None y 0.
+print()
