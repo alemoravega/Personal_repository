de8d151bd8c93b9a0850ceeab7d2f7ab16098965
donpollops5 <mora.vega.ale@gmail.com>
2023-12-26 14:21:27 -0300

    sigo con el resumen de python

--- Python/Unidad 1/2-2_Flujos_if.py
+++ Python/Unidad 1/2-2_Flujos_if.py
@@ -4,6 +4,28 @@
 # Version 0.0.0.0.0.0.1
 # DonPollo 2023 - All rights reserved
 
-# C O M P A R A D O R E S   L Ó G I C O S :
+# F L U J O S :  I F, F O R, W H I L E, E T C.
+
+# ********************************************************************************************
+
+# if, elif y else.
+
+# Condicional if: si se cumple la condición, se ejecuta x que esté anidado, si no se cumple,
+# se ejecuta lo demás. El orden de las instrucciones es importante.
+
+edad = int(input("¿Cuántos años tienes? "))
+if edad > 18:
+    print("Eres mayor de edad.")
+elif edad == 18:
+    Print("Tienes justo 18, eres mayor de edad.")
+else:
+    print("Eres menor de edad")
+
+# If Ternario
+
+# Otra forma de escribir los if y else:
+edad1 = int(input("Ingresa tu edad: "))
+mensaje1 = "es mayor de 17" if edad1 > 17 else "es menor"
+print(mensaje1)
 
 # ********************************************************************************************
