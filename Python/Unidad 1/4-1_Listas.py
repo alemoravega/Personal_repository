4460c33bcce037c83797a5c6b85a9244489f3d26
donpollops5 <mora.vega.ale@gmail.com>
2023-12-28 18:10:21 -0300

    sigo con el resumen de python

---
+++ Python/Unidad 1/4-1_Listas.py
@@ -0,0 +1,21 @@
+# *************************************************************************************************
+
+print(""" Resumen Puthon Aspectos básicos""")
+# Version 0.0.0.0.0.0.1
+# DonPollo 2023 - All rights reserved
+
+# L I S T A S .
+
+# *************************************************************************************************
+
+# Las listas son una sucesión de elementos, como listas (sublistas), números, strings, etc.
+
+# lista con 10 elementos (números):
+lista1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(lista1)
+# lista con 3 elementos (strings):
+lista2 = ["a", "b", "c"]
+print(lista2)
+# Lista con palabras (Strings):
+lista3 = ["Don", "Pollo", "es", "el", "mejor"]
+print(lista3)
